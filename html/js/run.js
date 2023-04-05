e2ea4bad6f9c750136705291d878b5c2d54c6dd1
김은영 <66293497+aldo814@users.noreply.github.com>
2023-04-05 10:08:40 +0900

    .

--- html/js/run.js
+++ html/js/run.js
@@ -91,14 +91,14 @@ $(document).ready(function () {
                 });
             }
 
-            let bWidth = window.innerWidth;
-            window.addEventListener("resize", () => {
-                const nWidth = window.innerWidth;
-                if ((bWidth < 1024 && nWidth >= 1024) || (bWidth > 1023 && nWidth <= 1023)) {
-                    location.reload();
-                }
-                beforeWidth = nowWidth;
-            });
+//            let bWidth = window.innerWidth;
+//            window.addEventListener("resize", () => {
+//                const nWidth = window.innerWidth;
+//                if ((bWidth < 1024 && nWidth >= 1024) || (bWidth > 1023 && nWidth <= 1023)) {
+//                    location.reload();
+//                }
+//                beforeWidth = nowWidth;
+//            });
 
 
 
